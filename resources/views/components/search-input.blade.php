5167554d7f2f0f6e2f01ed2aa4b3a44d62cac162
Ivan Meshcheryakov <meshcheryakovvrn@gmail.com>
2023-02-14 20:51:54 +0400

    Перевод для валидации

---
+++ resources/views/components/search-input.blade.php
@@ -0,0 +1,21 @@
+@props([
+    'disabled' => false,
+])
+
+<span class="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white">Search</span>
+<div class="relative flex items-center">
+    <div class="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
+        <svg aria-hidden="true" class="w-5 h-5 text-gray-500 dark:text-gray-400" fill="none" stroke="currentColor"
+             viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                  d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path>
+        </svg>
+    </div>
+
+    <input type="search" {{ $disabled ? 'disabled' : '' }} {!! $attributes->merge(['class' => 'pl-10 border-gray-300 dark:border-gray-700 dark:bg-gray-900 dark:text-gray-300 focus:border-indigo-500 dark:focus:border-indigo-600 focus:ring-indigo-500 dark:focus:ring-indigo-600 rounded-md shadow-sm']) !!}>
+    <button type="button"
+            x-on:click='enterAddressInput'
+            class="absolute right-2.5 bg-gray-800 dark:bg-gray-200 border border-transparent rounded-md font-semibold text-xs text-white dark:text-gray-800 uppercase tracking-widest hover:bg-gray-700 dark:hover:bg-white focus:bg-gray-700 dark:focus:bg-white active:bg-gray-900 dark:active:bg-gray-300 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 dark:focus:ring-offset-gray-800 transition ease-in-out duration-150 px-2 py-1">
+        Поиск
+    </button>
+</div>
